44ae075823ae0652d68a6b99eb3bcd723af149ae
Thiago Cordeiro Dantas <thiiaguinhocordeirodantas@gmail.com>
2023-04-14 19:56:27 -0300

    criando formulario

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,13 +1,12 @@
 import Banner from './components/Banner';
+import Formulario from './components/Formulario';
 import CampoTexto from './components/campoTexto';
 
 function App() {
   return (
     <div className="App">
-       <Banner/>
-       <CampoTexto label="Nome" placeholder="Digite o seu nome"/>
-       <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
-       <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem"/>
+      <Banner/>
+      <Formulario/>
     </div>
   );
 }
--- organo/src/components/Banner/banner.js
+++
@@ -1,9 +0,0 @@
-const Banner = () => {
-    //JSX
-    return (
-        <header className="banner">
-             <img src="/img/banner.png" alt="O banner principal da pargina do Organo"/>
-        </header>
-    )}
-
-
