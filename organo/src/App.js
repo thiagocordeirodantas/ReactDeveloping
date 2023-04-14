a6256ca1d365243f0c8f2aba2db67b6dd5d8a85d
Thiago Cordeiro Dantas <thiiaguinhocordeirodantas@gmail.com>
2023-04-14 14:05:08 -0300

    criando componente banner

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,20 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from './components/Banner/banner';
 
 function App() {
   return (
     <div className="App">
+       <Banner/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
-        <p>Bem Vindo ao React</p>
+       
+        <p>Bem vindo</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          
         </a>
       </header>
     </div>
---
+++ organo/src/components/Banner/banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner () {
+    //JSX
+    return (
+        <header className="banner">
+             <img src="/img/banner.png" alt="O banner principal da pargina do Organo"/>
+        </header>
+    )}
+
+
+export default Banner
