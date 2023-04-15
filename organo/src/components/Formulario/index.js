97596c7be35bf281da03550201025a27a84dcce5
Thiago Cordeiro Dantas <thiaguinhocordeirodanttas@gmail.com>
2023-04-15 20:36:46 -0300

    criando a lista dos times

--- organo/src/components/Formulario/index.js
+++ organo/src/components/Formulario/index.js
@@ -1,7 +1,19 @@
 import CampoTexto from '../campoTexto'
 import './Formulario.css'
+import ListaSuspensa from '../ListaSuspensa'
 
 const Formulario = () => {
+
+    const times =[
+        'Programacao',
+        'Front-End',
+        'Data Science',
+        'DevOps',
+        'UX e Design',
+        'Mobile',
+        'Inovacao e Gestao'
+    ]
+
     return (
         <section className="formulario">
             <form>
@@ -9,9 +21,10 @@ const Formulario = () => {
                 <CampoTexto label="Nome" placeholder="Digite o seu nome" />
                 <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
+                <ListaSuspensa label="Time" itens={times}/> 
             </form>
         </section>
     )
 }
 
-export default Formulario
+export default Formulario 
