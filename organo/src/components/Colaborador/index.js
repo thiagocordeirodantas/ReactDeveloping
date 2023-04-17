5e1bdd5c2cb1d572416a52bea6cfe298c92f0ed1
Thiago Cordeiro Dantas <thiaguinhocordeirodanttas@gmail.com>
2023-04-16 22:22:23 -0300

    criando card colaboraador

---
+++ organo/src/components/Colaborador/index.js
@@ -0,0 +1,18 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>  
+            <img src='https://github.com/thiagocordeirodantas.png' alt="Thiago Cordeiro"/>
+            </div>
+            <div className='rodape'>
+                <h4>Thiago Cordeiro</h4>
+                <h5>Estudante front-end</h5>
+            </div>
+        </div>
+    )
+}
+
+
+export default Colaborador
