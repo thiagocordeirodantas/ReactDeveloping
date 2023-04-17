ddb69636075337ddc0273d78f46361f3f546dc90
Thiago Cordeiro Dantas <thiaguinhocordeirodanttas@gmail.com>
2023-04-16 23:03:57 -0300

    projeto finalizado

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,15 +1,14 @@
 import { useState } from 'react';
-import Banner from './components/Banner';
-import Formulario from './components/Formulario';
-import CampoTexto from './components/campoTexto';
-import Time from './components/Time';
+import Banner from './componentes/Banner';
+import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App() {
 
   const times = [
     {
-      nome: 'Programacao',
-      corPrimaria: '#57C271',
+      nome: 'Programação',
+      corPrimaria: '#57C278',
       corSecundaria: '#D9F7E9'
     },
     {
@@ -20,42 +19,50 @@ function App() {
     {
       nome: 'Data Science',
       corPrimaria: '#A6D157',
-       corSecundaria: '#F0F8E2'
+      corSecundaria: '#F0F8E2'
     },
     {
-      nome: 'DevOps',
+      nome: 'Devops',
       corPrimaria: '#E06B69',
-       corSecundaria: '#FDE7E8'
+      corSecundaria: '#FDE7E8'
     },
     {
       nome: 'UX e Design',
-       corPrimaria: '#DB6EBF',
-       corSecundaria: '#FAE9F5'
+      corPrimaria: '#DB6EBF',
+      corSecundaria: '#FAE9F5'
     },
     {
       nome: 'Mobile',
-        corPrimaria: '#FFBA05',
-       corSecundaria: '#FFF5D9'
+      corPrimaria: '#FFBA05',
+      corSecundaria: '#FFF5D9'
     },
     {
-      nome: 'Inovacao e Gestao',
-        corPrimaria: '#FF8A29',
-       corSecundaria: '#FFEEDF'
-    },
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#FF8A29',
+      corSecundaria: '#FFEEDF'
+    }
   ]
 
-const [colaboradores, setColaboradores] = useState([])
+  const [colaboradores, setColaboradores] = useState([])
 
-const aoNovoColaboradorAdicionado = (colaborador) => {
-  console.log(colaborador)
-}
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    debugger
+    setColaboradores([...colaboradores, colaborador])
+  }
 
   return (
     <div className="App">
-      <Banner/>
+      <Banner />
       <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
-      {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria}/> )}
-  
+
+      {times.map(time => <Time 
+        key={time.nome} 
+        nome={time.nome} 
+        corPrimaria={time.corPrimaria} 
+        corSecundaria={time.corSecundaria} 
+        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+      />)}   
+
     </div>
   );
 }
